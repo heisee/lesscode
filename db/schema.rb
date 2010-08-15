a67f10ad85613873c7c483d7e70c0b030dfe827c
Heiko Seebach <heiko.seebach@web.de>
2010-08-15 22:46:39 +0200

    chef-cookbooks und grundlegende funktionen

--- db/schema.rb
+++ db/schema.rb
@@ -1,21 +1,42 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead 
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your 
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100718140724) do
+ActiveRecord::Schema.define(:version => 20100815113841) do
+
+  create_table "delmes", :force => true do |t|
+    t.integer  "a"
+    t.string   "b"
+    t.date     "c"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "profiles", :force => true do |t|
-    t.string   "name"
-    t.string   "zip_code"
-    t.string   "location"
+    t.string   "person_name"
     t.string   "company_name"
+    t.string   "street"
+    t.string   "homepage"
+    t.string   "available_from_type",     :default => "depends", :null => false
+    t.date     "available_from_date"
+    t.string   "available_duration_type", :default => "depends", :null => false
+    t.date     "available_to_date"
+    t.string   "zip_code"
+    t.text     "about"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "mobile"
+    t.string   "email"
+    t.string   "type",                                           :null => false
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
